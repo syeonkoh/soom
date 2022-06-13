84e77e73887f2f96631748dca6387bf4c89b9633
syeonkoh <syeonko12@gmail.com>
2022-06-13 18:33:03 -0400

    nickname and basic chat

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import http from "http";
 import WebSocket from "ws";
 import express from "express";
+
 const app = express();
 
 app.set('view engine', "pug");
@@ -9,28 +10,32 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_,res) => res.render("home"));
 app.get("/*", (_,res) => res.redirect("/"));
 
-console.log("hello");
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 
 const server = http.createServer(app);
-
 const wss = new WebSocket.Server({ server });
 
 function onSocketClose() {
     console.log("Disconnected from the Browser ❌");
 }
 
-function onSocketMessage(message) {
-    console.log(message.toString('utf8'));
-}
 const sockets = [];
 
 wss.on("connection", (socket) => {
     sockets.push(socket);
+    socket["nickname"] = "Anon";
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
-    socket.on("message", (message) => {
-        sockets.forEach((aSocket) => aSocket.send(message));
+    socket.on("message", (msg) => {
+        const message = JSON.parse(msg);
+        switch (message.type) {
+          case "new_message":
+            sockets.forEach((aSocket) =>
+              aSocket.send(`${socket.nickname}: ${message.payload}`)
+            );
+          case "nickname":
+            socket["nickname"] = message.payload;
+        }
     });
 });
 
