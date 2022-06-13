ecf2f5fd41b427db0ca19b37d0324fad3e7ac6f7
syeonkoh <syeonko12@gmail.com>
2022-06-13 17:52:45 -0400

    1.7

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const app = express();
 app.set('view engine', "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-app.get("/", (req,res) => res.render("home"));
-app.get("/*", (req,res) => res.render("/"));
+app.get("/", (_,res) => res.render("home"));
+app.get("/*", (_,res) => res.redirect("/"));
 
 console.log("hello");
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
@@ -23,12 +23,15 @@ function onSocketClose() {
 function onSocketMessage(message) {
     console.log(message.toString('utf8'));
 }
+const sockets = [];
 
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
-    socket.on("message", onSocketMessage);
-    socket.send("hello!!!");
+    socket.on("message", (message) => {
+        sockets.forEach((aSocket) => aSocket.send(message));
+    });
 });
 
 server.listen(3000, handleListen);
